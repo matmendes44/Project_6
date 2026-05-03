a250608afdabc8e353ad217cfb6b92df22ed4e3f
jay <jayfisherr717@gmail.com>
2026-05-03 18:05:54 -0400

    listener added execution part for the keys

--- listener.py
+++ listener.py
@@ -3,17 +3,46 @@ from shortcut_manager import load_shortcuts
 from actions import execute_action
 
 held_keys = set()
+data=load_shortcuts()
 
+
+#converts ctrl alt y into a set we can compare against held keys 
+def parse_shortcut(keys_str):
+    parsed_result = set()
+    for part in keys_str.lower().split("+"):
+        part = part.strip()
+        if part =="ctrl":
+            parsed_result.add(keyboard.Key.ctrl_l)
+
+        elif part == "alt":
+            parsed_result.add(keyboard.Key.alt_l)
+        
+        elif part == "shift":
+            parsed_result.add(keyboard.Key.shift_l)
+        
+        else:#might need more of these cases if we add more key binds
+            parsed_result.add(keyboard.KeyCode.from_char(part))
+        return parsed_result
+
+#on press of a key add it to the set of held keys 
+#if the keys held == parsed_shortcuts so if you hold 
+#ctrl + n + W the parsed version would be ctrl n w 
+#if they are equal i.e. [ctrl n w] == [ctrl n w]
+#execute the action
 def on_press(key):
     held_keys.add(key)
-    print("keys pressed: "+ held_keys)
+    for shortcut in data["shortcuts"]:
+        if held_keys == parse_shortcut(shortcut["keys"]):
+            execute_action(shortcut["type"], shortcut["action"])
 
+#once you lift your finger off that key we do not care about it 
+#becuase it can not be used for a shortcut 
+#so remove the key from the set. 
 def on_release(key):
     held_keys.discard(key)#cant use remove becuase it can cauase an error i believe
 
 
 def main():
-    data=load_shortcuts()
     listen = keyboard.Listener(on_press=on_press, on_release=on_release)
     listen.start()
 
